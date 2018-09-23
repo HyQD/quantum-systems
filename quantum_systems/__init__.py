e249d7a8c5551865c4aa0c3015f3b2d4558d947e
Mint-kontor <oyvindschoyen@gmail.com>
2018-09-23 17:34:56 +0200

    Add electron gas to top module

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -2,3 +2,4 @@ from .system import QuantumSystem
 from quantum_systems.quantum_dots.one_dim.one_dim_ho import (
     OneDimensionalHarmonicOscillator
 )
+from quantum_systems.electron_gas.heg import HomogeneousElectronGas
