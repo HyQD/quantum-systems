e2e0d5bbf767af360a22519265d1aa4a847f5455
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-10-04 12:36:57 +0200

    Add tests of the two-dimensional quantum dot

---
+++ tests/conftest.py
@@ -0,0 +1,156 @@
+import os
+import pytest
+import numpy as np
+import numba
+
+l = 72
+filename_one_body = os.path.join(
+    "tests", "dat", "two_dim_quantum_dots_one_body_elements.dat"
+)
+filename_two_body = os.path.join(
+    "tests", "dat", "two_dim_quantum_dots_coulomb_elements.dat"
+)
+
+
+def pytest_namespace():
+    return {"l": l}
+
+
+@numba.njit(cache=True)
+def spin_delta(p, q):
+    return ((p & 0x1) ^ (q & 0x1)) ^ 0x1
+
+
+@numba.njit(cache=True)
+def antisymmetrize_orbital_integrals(oi):
+    l = len(oi) * 2
+
+    u = np.zeros((l, l, l, l), dtype=np.complex128)
+
+    for p in range(l):
+        _p = p // 2
+        for q in range(l):
+            _q = q // 2
+            for r in range(l):
+                _r = r // 2
+                for s in range(l):
+                    _s = s // 2
+
+                    u_pqrs = (
+                        spin_delta(p, r) * spin_delta(q, s) * oi[_p, _q, _r, _s]
+                    )
+
+                    u_pqsr = (
+                        spin_delta(p, s) * spin_delta(q, r) * oi[_p, _q, _s, _r]
+                    )
+
+                    u[p, q, r, s] = u_pqrs - u_pqsr
+
+    return u
+
+
+def get_file_orbital_integrals(l, filename):
+    orbital_integrals = np.zeros((l, l, l, l), dtype=np.complex128)
+
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.split()
+
+            if not line:
+                continue
+
+            p, q, r, s, val = line
+
+            if any(filter(lambda x: int(x) >= l, (p, q, r, s))):
+                continue
+
+            orbital_integrals[int(p), int(q), int(r), int(s)] = float(val)
+
+    return orbital_integrals
+
+
+_orbital_integrals = get_file_orbital_integrals(l // 2, filename_two_body)
+_u = antisymmetrize_orbital_integrals(_orbital_integrals)
+
+
+def get_file_one_body_elements(l, filename):
+    hi = np.zeros((l, l), dtype=np.complex128)
+
+    with open(filename, "r") as f:
+        f.readline()
+        for line in f:
+            line = line.split()
+
+            if not line:
+                continue
+
+            p, val = line
+
+            if int(p) >= l:
+                continue
+
+            hi[int(p), int(p)] = float(val)
+
+    return hi
+
+
+@numba.njit(cache=True)
+def get_antisymmetrized_one_body_elements(hi):
+    l = len(hi) * 2
+
+    h = np.zeros((l, l), dtype=np.complex128)
+    for p in range(l):
+        _p = p // 2
+        for q in range(l):
+            _q = q // 2
+
+            h[p, q] = spin_delta(p, q) * hi[_p, _q]
+
+    return h
+
+
+_hi = get_file_one_body_elements(l // 2, filename_one_body)
+_h = get_antisymmetrized_one_body_elements(_hi)
+
+
+def read_index_map():
+    path = os.path.join("tests", "dat", "index_map.dat")
+    index_map = []
+
+    with open(path, "r") as f:
+        f.readline()
+        for line in f:
+            p, n, m = map(lambda x: int(x), line.split())
+            index_map.append((n, m))
+
+            assert p == len(index_map) - 1
+
+    return index_map
+
+
+_index_map = read_index_map()
+
+
+@pytest.fixture
+def index_map():
+    return _index_map
+
+
+@pytest.fixture
+def hi():
+    return _hi
+
+
+@pytest.fixture
+def h():
+    return _h
+
+
+@pytest.fixture
+def orbital_integrals():
+    return _orbital_integrals
+
+
+@pytest.fixture
+def u():
+    return _u
---
+++ tests/test_two_dim_ho.py
@@ -0,0 +1,52 @@
+import pytest
+import numpy as np
+
+from quantum_systems.quantum_dots.two_dim.two_dim_interface import (
+    get_index_p,
+    get_indices_nm,
+    get_one_body_elements,
+    get_coulomb_elements,
+)
+
+from quantum_systems.system_helper import (
+    get_antisymmetrized_one_body_elements,
+    get_antisymmetrized_two_body_elements,
+)
+
+
+def test_p_index(index_map):
+    for p, (n, m) in enumerate(index_map):
+        assert p == get_index_p(n, m)
+
+
+def test_nm_indices(index_map):
+    for p, (n, m) in enumerate(index_map):
+        assert get_indices_nm(p) == (n, m)
+
+
+def test_one_body_elements(hi):
+    l = pytest.l // 2
+    _hi = get_one_body_elements(l)
+
+    np.testing.assert_allclose(hi, _hi, atol=1e-6, rtol=1e-6)
+
+
+def test_antisymmetric_one_body_elements(h):
+    l = pytest.l
+    _h = get_antisymmetrized_one_body_elements(get_one_body_elements(l // 2))
+
+    np.testing.assert_allclose(h, _h, atol=1e-6, rtol=1e-6)
+
+
+def test_two_body_elements(orbital_integrals):
+    l = pytest.l // 2
+    oi = get_coulomb_elements(l)
+
+    np.testing.assert_allclose(orbital_integrals, oi, atol=1e-6, rtol=1e-6)
+
+
+def test_antisymmetric_two_body_elements(u):
+    l = pytest.l
+    _u = get_antisymmetrized_two_body_elements(get_coulomb_elements(l // 2))
+
+    np.testing.assert_allclose(u, _u, atol=1e-6, rtol=1e-6)
