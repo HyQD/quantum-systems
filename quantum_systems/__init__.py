39e225b872991e8c0b7e76d63da83100d8719dfd
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-10-04 12:14:40 +0200

    Add two-dim-ho to top module

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -2,4 +2,7 @@ from .system import QuantumSystem
 from quantum_systems.quantum_dots.one_dim.one_dim_ho import (
     OneDimensionalHarmonicOscillator
 )
+from quantum_systems.quantum_dots.two_dim.two_dim_ho import (
+    TwoDimensionalHarmonicOscillator
+)
 from quantum_systems.electron_gas.heg import HomogeneousElectronGas
