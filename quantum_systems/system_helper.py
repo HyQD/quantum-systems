265b7f3885bb91c90eaf9f409346275be3d725b3
Schoyen-Linux <oyvinssc@student.matnat.uio.no>
2018-12-14 18:55:46 +0100

    Fix naming of system helper

--- quantum_systems/system_helper.py
+++ quantum_systems/system_helper.py
@@ -12,14 +12,14 @@ def spin_delta(p, q):
     return ((p & 0x1) ^ (q & 0x1)) ^ 0x1
 
 
-def new_transform_one_body_elements(h, c):
+def transform_one_body_elements(h, c):
     _h = np.dot(h, c)
     _h = np.dot(c.conj().T, _h)
 
     return _h
 
 
-def new_transform_two_body_elements(u, c):
+def transform_two_body_elements(u, c):
     _u = np.dot(u, c)
     _u = np.tensordot(_u, c, axes=(2, 0)).transpose(0, 1, 3, 2)
     _u = np.tensordot(_u, c.conj(), axes=(1, 0)).transpose(0, 3, 1, 2)
@@ -28,11 +28,11 @@ def new_transform_two_body_elements(u, c):
     return _u
 
 
-def add_spin_h(h):
+def add_spin_one_body(h):
     return np.kron(h, np.eye(2))
 
 
-def add_spin_u(_u):
+def add_spin_two_body(_u):
     u = _u.transpose(1, 3, 0, 2)
     u = np.kron(u, np.eye(2))
     u = u.transpose(2, 3, 0, 1)
@@ -42,5 +42,5 @@ def add_spin_u(_u):
     return u
 
 
-def antisymmetrize_u(_u):
+def anti_symmetrize_u(_u):
     return _u - _u.transpose(0, 1, 3, 2)
