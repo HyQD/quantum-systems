effd238c5c6a1da83f0c012e0b30bac109798b7f
Mint-kontor <oyvindschoyen@gmail.com>
2018-12-17 12:48:48 +0100

    Black __init__

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -1,9 +1,9 @@
 from .system import QuantumSystem
 from .custom_system import CustomSystem
 from quantum_systems.quantum_dots.one_dim.one_dim_ho import (
-    OneDimensionalHarmonicOscillator,
+    OneDimensionalHarmonicOscillator
 )
 from quantum_systems.quantum_dots.two_dim.two_dim_ho import (
-    TwoDimensionalHarmonicOscillator,
+    TwoDimensionalHarmonicOscillator
 )
 from quantum_systems.electron_gas.heg import HomogeneousElectronGas
