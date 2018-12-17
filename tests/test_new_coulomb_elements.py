9661de56e1570fbf7d5643ffc508c2d181c6a067
Mint-kontor <oyvindschoyen@gmail.com>
2018-12-17 12:49:28 +0100

    Fix error in for-loop

--- tests/test_new_coulomb_elements.py
+++ tests/test_new_coulomb_elements.py
@@ -6,17 +6,17 @@ from quantum_systems.quantum_dots.two_dim.coulomb_elements import new_coulomb_ho
 
 
 def test_comparison():
-    l = 90
-    for i in range(l):
-        n_i, m_i = get_indices_nm(i)
-        for j in range(l):
-            n_j, m_j = get_indices_nm(j)
-            for k in range(l):
-                n_k, m_k = get_indices_nm(k)
-                for l in range(l):
-                    n_l, m_l = get_indices_nm(l)
+    l = 20
+    for p in range(l):
+        n_p, m_p = get_indices_nm(p)
+        for q in range(l):
+            n_q, m_q = get_indices_nm(q)
+            for r in range(l):
+                n_r, m_r = get_indices_nm(r)
+                for s in range(l):
+                    n_s, m_s = get_indices_nm(s)
 
-                    args = [n_i, m_i, n_j, m_j, n_k, m_k, n_l, m_l]
+                    args = [n_p, m_p, n_q, m_q, n_r, m_r, n_s, m_s]
 
                     assert (
                         abs(get_coulomb_element(*args) - new_coulomb_ho(*args))
