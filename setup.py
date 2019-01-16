41b1434efb3428508600e57afda1745075262e0f
Sebastian Gregorius Winther-Larsen <sebastwi@uio.no>
2019-01-16 19:55:13 +0100

    Update version so can be noticed by other modules

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="Quantum systems", version="0.0.1", packages=find_packages())
+setup(name="Quantum systems", version="0.0.2", packages=find_packages())
