ef0db2079cff9b18fecedd34b0b09f4319b65d0e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-07 20:24:56 +0100

    Add run of one dimensional double-well system

--- scripts/plot_one_dim_ho.py
+++ scripts/plot_one_dim_ho.py
@@ -2,20 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from quantum_systems import OneDimensionalHarmonicOscillator
+from quantum_systems.quantum_dots.one_dim.one_dim_potentials import DWPotential
 
+
+mass = 1
+omega = 1
+length_of_dw = 5
 
 n = 2
-l = 6
-grid_length = 10
+l = 20
+grid_length = 5
 num_grid_points = 1001
 
-odho = OneDimensionalHarmonicOscillator(n, l, grid_length, num_grid_points)
-odho.setup_system()
+odho = OneDimensionalHarmonicOscillator(
+    n, l, grid_length, num_grid_points, mass=mass, omega=omega
+)
+odho.setup_system(potential=DWPotential(mass, omega, length_of_dw))
 
-plt.plot(odho.grid, odho.spf[0].real ** 2)
-plt.plot(odho.grid, odho.spf[1].real ** 2)
-plt.plot(odho.grid, odho.spf[2].real ** 2)
-plt.show()
+for i in range(l // 2):
+    plt.plot(odho.grid, odho.spf[i].real ** 2 + odho.eigen_energies[i])
 
-plt.plot(odho.grid, odho.potential(odho.grid))
+plt.plot(odho.grid, odho.potential(odho.grid), label=r"$v_{DW}$")
+plt.legend(loc="best")
 plt.show()
