744c9e5a246d2427430a1e0d036b89d9bc80b75c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-07 20:24:46 +0100

    Add double-well potential

--- quantum_systems/quantum_dots/one_dim/one_dim_potentials.py
+++ quantum_systems/quantum_dots/one_dim/one_dim_potentials.py
@@ -14,3 +14,14 @@ class HOPotenial(OneDimPotential):
 
     def __call__(self, x):
         return 0.5 * self.mass * self.omega ** 2 * x ** 2
+
+
+class DWPotential(HOPotenial):
+    def __init__(self, mass, omega, l):
+        super().__init__(mass, omega)
+        self.l = l
+
+    def __call__(self, x):
+        return super().__call__(x) + 0.5 * self.mass * self.omega ** 2 * (
+            0.25 * self.l ** 2 - self.l * abs(x)
+        )
