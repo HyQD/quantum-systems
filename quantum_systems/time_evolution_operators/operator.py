c268bb2d17008f4d5735d53357ffdb299cf57243
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-21 14:48:30 +0100

    Fix LaserField-operator

--- quantum_systems/time_evolution_operators/operator.py
+++ quantum_systems/time_evolution_operators/operator.py
@@ -11,24 +11,25 @@ class TimeEvolutionOperator:
 
 class LaserField(TimeEvolutionOperator):
     def __init__(self, laser_pulse, polarization_vector=None):
-        np = self._system.np
-
-        self._laser_pulse = (
-            laser_pulse if callable(laser_pulse) else lambda t: laser_pulse
-        )
-
-        if polarization_vector is None:
-            # Set default polarization fully along x-axis
-            polarization_vector = np.zeros(self._system.dipole_moment.shape[0])
-            polarization_vector[0] = 1
-
-        self._polarization = (
-            polarization_vector
-            if callable(polarization_vector)
-            else lambda t: polarization_vector
-        )
+        self._laser_pulse = laser_pulse
+        self._polarization = polarization_vector
 
     def h_t(self, current_time):
+        np = self._system.np
+
+        if not callable(self._laser_pulse):
+            tmp = self._laser_pulse
+            self._laser_pulse = lambda t: tmp
+
+        if self._polarization is None:
+            # Set default polarization along x-axis
+            self._polarization = np.zeros(self._system.dipole_moment.shape[0])
+            self._polarization[0] = 1
+
+        if not callable(self._polarization):
+            tmp = self._polarization
+            self._polarization = lambda t: tmp
+
         return self._system.h + self._laser_pulse(current_time) * np.tensordot(
             self._polarization(current_time),
             self._system.dipole_moment,
