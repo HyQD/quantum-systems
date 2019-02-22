d107f36ec505db91ef03133d187ebc3b497a34e7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-22 19:59:35 +0100

    Update push_formatting.sh

--- push_formatting.sh
+++ push_formatting.sh
@@ -6,6 +6,7 @@ user_email='travis@travis-ci.org'
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git -c user.name="$user_name" -c user.email="$user_email" commit -m "Black files"
+    git remote rm origin
     git remote add origin https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG > /dev/null 2>&1
     git push origin $TRAVIS_BRANCH --quiet
 fi
