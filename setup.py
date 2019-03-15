922440ea9990be0750d3f54a2df0b90cd76cba60
gregwinther <sebastwi@uio.no>
2019-03-15 09:43:47 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="Quantum systems", version="0.0.2", packages=find_packages())
+setup(name="Quantum systems", version="0.0.3", packages=find_packages())
