2d442348c9786b7229bd4bc22d43768276fa75ec
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-04-03 15:16:15 +0200

    Finish setup of double-well system

--- quantum_systems/quantum_dots/two_dim/two_dim_dw.py
+++ quantum_systems/quantum_dots/two_dim/two_dim_dw.py
@@ -3,7 +3,15 @@ import math
 
 from quantum_systems import QuantumSystem
 from quantum_systems.quantum_dots.two_dim.two_dim_helper import (
-    get_one_body_elements,
+    get_double_well_one_body_elements,
+    get_coulomb_elements,
+    spf_state,
+)
+from quantum_systems.system_helper import (
+    add_spin_one_body,
+    add_spin_two_body,
+    anti_symmetrize_u,
+    transform_two_body_elements,
 )
 
 
@@ -46,7 +54,7 @@ class TwoDimensionalDoubleWell(QuantumSystem):
         radius,
         num_grid_points,
         barrier_strength=1,
-        l_ho_factor=1.3,
+        l_ho_factor=1.0,
         omega=1,
         mass=1,
     ):
@@ -64,10 +72,26 @@ class TwoDimensionalDoubleWell(QuantumSystem):
         self.radius = radius
         self.num_grid_points = num_grid_points
 
-        self.radius = np.linspace(0, self.radius_length, self.num_grid_points)
+        self.radius = np.linspace(0, self.radius, self.num_grid_points)
         self.theta = np.linspace(0, 2 * np.pi, self.num_grid_points)
 
     def setup_system(self, axis=0):
+        """Function setting up the one- and two-body elements, the
+        single-particle functions, dipole moments and other quantities used by
+        second quantization methods.
+        Parameters
+        ----------
+        axis : int
+            Specifies which axis the barrier should be set to. For axis == 0
+            this corresponds to the x-axis and axis == 1 the y-axis. That is,
+                axis == 0 -> <p||x||q>,
+                axis == 1 -> <p||y||q>.
+
+        Returns
+        -------
+        None
+        """
+
         __h = get_double_well_one_body_elements(
             self.l_ho // 2,
             self.omega,
@@ -81,3 +105,32 @@ class TwoDimensionalDoubleWell(QuantumSystem):
         )
 
         self.epsilon, C = np.linalg.eigh(__h)
+
+        # Construct one-body matrix from the eigenenergies of the single
+        # particle eigenstates for the double well. We only include the
+        # states we deem converged.
+        _h = np.diagflat(self.epsilon[: self.l // 2])
+        self._h = add_spin_one_body(_h, np=np)
+
+        # Transform Coulomb elements to new double-well basis
+        _u = transform_two_body_elements(__u, C[:, : self.l // 2], np=np)
+        self._u = anti_symmetrize_u(add_spin_two_body(_u, np=np))
+
+        # Create harmonic oscillator single-particle functions
+        _spf_ho = self.setup_spf()
+        # Transform to double-well basis
+        _spf_dw = np.tensordot(C[:, : self.l // 2], _spf_ho, axes=((0), (0)))
+
+        # Create spin-degenerate single-particle functions
+        self._spf = np.zeros((self.l, *self.R.shape), dtype=np.complex128)
+        self._spf[::2] += _spf_dw
+        self._spf[1::2] += _spf_dw
+
+    def setup_spf(self):
+        self.R, self.T = np.meshgrid(self.radius, self.theta)
+        spf = np.zeros((self.l_ho // 2, *self.R.shape), dtype=np.complex128)
+
+        for p in range(self.l_ho // 2):
+            spf[p, :] += spf_state(self.R, self.T, p, self.mass, self.omega)
+
+        return spf
