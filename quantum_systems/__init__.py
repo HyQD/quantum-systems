92431ff2fba65ffea617e6c771ff0498bd999c7d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-04-03 15:04:58 +0200

    Import two-dim double well to top module

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -6,4 +6,7 @@ from quantum_systems.quantum_dots.one_dim.one_dim_ho import (
 from quantum_systems.quantum_dots.two_dim.two_dim_ho import (
     TwoDimensionalHarmonicOscillator,
 )
+from quantum_systems.quantum_dots.two_dim.two_dim_dw import (
+    TwoDimensionalDoubleWell,
+)
 from quantum_systems.electron_gas.heg import HomogeneousElectronGas
