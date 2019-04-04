55cab6746d528772168520de667cb0c13029e5d2
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-04-04 12:52:48 +0200

    Add test of manual basis change

--- tests/test_two_dim_dw.py
+++ tests/test_two_dim_dw.py
@@ -4,6 +4,10 @@ from quantum_systems import (
     TwoDimensionalDoubleWell,
     TwoDimensionalHarmonicOscillator,
 )
+from quantum_systems.quantum_dots.two_dim.two_dim_helper import (
+    get_double_well_one_body_elements,
+)
+from quantum_systems.system_helper import add_spin_one_body
 
 
 def test_zero_barrier():
@@ -51,3 +55,49 @@ def test_spf_energies():
     )
     tddw.setup_system(axis=1)
     np.testing.assert_allclose(tddw.epsilon, test_energies)
+
+
+def test_change_of_basis():
+    # We try to construct a two-dimensional double-well system from a
+    # two-dimensional harmonic oscillator system by using the change-of-basis
+    # function with C found from diagonalizing the double-well one-body
+    # Hamiltonian.
+    n = 2
+    l = 12
+    l_ho_factor = 2
+    l_ho = int(l * l_ho_factor)
+    omega = 1
+    mass = 1
+    barrier_strength = 3
+    radius = 10
+    num_grid_points = 401
+    axis = 0
+
+    tdho = TwoDimensionalHarmonicOscillator(
+        n, l_ho, radius, num_grid_points, omega=omega
+    )
+    tdho.setup_system()
+
+    h_dw = get_double_well_one_body_elements(
+        l_ho // 2, omega, mass, barrier_strength, dtype=np.complex128, axis=axis
+    )
+
+    epsilon, C_dw = np.linalg.eigh(h_dw)
+    C = add_spin_one_body(C_dw, np=np)
+
+    tdho.change_basis(C[:, :l])
+
+    tddw = TwoDimensionalDoubleWell(
+        n,
+        l,
+        radius,
+        num_grid_points,
+        omega=omega,
+        mass=mass,
+        barrier_strength=barrier_strength,
+        l_ho_factor=l_ho_factor,
+    )
+    tddw.setup_system(axis=axis)
+
+    np.testing.assert_allclose(tdho.u, tddw.u, atol=1e-7)
+    np.testing.assert_allclose(tdho.spf, tddw.spf, atol=1e-7)
