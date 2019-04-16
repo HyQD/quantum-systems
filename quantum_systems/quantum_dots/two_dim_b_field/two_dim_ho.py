805f7f60be7a95c42b5de0328ab34f3d83a22768
Travis CI <travis@travis-ci.org>
2019-04-16 11:29:44 +0000

    Black files

--- quantum_systems/quantum_dots/two_dim_b_field/two_dim_ho.py
+++ quantum_systems/quantum_dots/two_dim_b_field/two_dim_ho.py
@@ -51,9 +51,9 @@ class TwoDimHarmonicOscB(QuantumSystem):
             n_array, m_array, omega_c=self.omega_c, omega=self.omega
         )
 
-        self.__h = get_one_body_elements(
-            num_orbitals, df=self.df
-        ).astype(np.complex128)
+        self.__h = get_one_body_elements(num_orbitals, df=self.df).astype(
+            np.complex128
+        )
         self.__u = np.sqrt(self.omega) * get_coulomb_elements(
             num_orbitals, df=self.df
         ).astype(np.complex128)
