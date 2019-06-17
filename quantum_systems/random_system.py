18bbafac99192e127b9c397e86cdedbf3ed4c625
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-06-17 14:32:27 +0200

    Use default values from CustomSystem
        The user has to explicitly request spin-redundancy and
        anti-symmetric elements.

--- quantum_systems/random_system.py
+++ quantum_systems/random_system.py
@@ -8,16 +8,22 @@ class RandomSystem(CustomSystem):
     specific systems.
     """
 
-    def setup_system(self):
+    def setup_system(self, **kwargs):
         """Function creating a system consisting of random matrix elements."""
 
-        self.set_h()
-        self.set_u()
-        self.set_s()
-        self.set_dipole_moment()
+        u_kwargs = kwargs.copy()
+
+        # Remove the anti-symmetrize argument for the one-body integrals
+        if kwargs.get("anti_symmetrize") is not None:
+            kwargs.pop("anti_symmetrize")
+
+        self.set_h(**kwargs)
+        self.set_u(**u_kwargs)
+        self.set_s(**kwargs)
+        self.set_dipole_moment(**kwargs)
         self.set_nuclear_repulsion_energy()
 
-    def set_h(self, *args, **kwargs):
+    def set_h(self, **kwargs):
         """Function setting up random one-body Hamiltonian matrix elements. The
         arguments are the same as for the CustomSystem-class.
 
@@ -26,14 +32,15 @@ class RandomSystem(CustomSystem):
         CustomSystem.set_h : Set one-body Hamiltonian.
         """
 
-        if len(args) == 0:
-            shape = (self.l // 2, self.l // 2)
-            args = [get_random_elements(shape, self.np)]
-            kwargs["add_spin"] = True
+        shape = (self.l, self.l)
 
+        if kwargs.get("add_spin"):
+            shape = (self.l // 2, self.l // 2)
+
+        args = [get_random_elements(shape, self.np)]
         super().set_h(*args, **kwargs)
 
-    def set_u(self, *args, **kwargs):
+    def set_u(self, **kwargs):
         """Function setting up random two-body Hamiltonian matrix elements. The
         arguments are the same as for the CustomSystem-class.
 
@@ -42,15 +49,15 @@ class RandomSystem(CustomSystem):
         CustomSystem.set_u : Set two-body Hamiltonian.
         """
 
-        if len(args) == 0:
-            shape = (self.l // 2, self.l // 2, self.l // 2, self.l // 2)
-            u = get_random_elements(shape, self.np)
-            u = 0.5 * (u + u.transpose(1, 0, 3, 2))
+        shape = [self.l for i in range(4)]
 
-            args = [u]
-            kwargs["add_spin"] = True
-            kwargs["anti_symmetrize"] = True
+        if kwargs.get("add_spin"):
+            shape = [self.l // 2 for i in range(4)]
 
+        u = get_random_elements(shape, self.np)
+        u = 0.5 * (u + u.transpose(1, 0, 3, 2))
+
+        args = [u]
         super().set_u(*args, **kwargs)
 
     def set_s(self, *args, **kwargs):
@@ -62,11 +69,12 @@ class RandomSystem(CustomSystem):
         CustomSystem.set_s : Set overlap matrix elements.
         """
 
-        if len(args) == 0:
-            shape = (self.l // 2, self.l // 2)
-            args = [get_random_elements(shape, self.np)]
-            kwargs["add_spin"] = True
+        shape = (self.l, self.l)
 
+        if kwargs.get("add_spin"):
+            shape = (self.l // 2, self.l // 2)
+
+        args = [get_random_elements(shape, self.np)]
         super().set_s(*args, **kwargs)
 
     def set_dipole_moment(self, *args, dim=3, **kwargs):
@@ -83,11 +91,12 @@ class RandomSystem(CustomSystem):
         CustomSystem.set_dipole_moment : Set dipole moment matrix elements.
         """
 
-        if len(args) == 0:
-            shape = (dim, self.l // 2, self.l // 2)
-            args = [get_random_elements(shape, self.np)]
-            kwargs["add_spin"] = True
+        shape = (dim, self.l, self.l)
 
+        if kwargs.get("add_spin"):
+            shape = (dim, self.l // 2, self.l // 2)
+
+        args = [get_random_elements(shape, self.np)]
         super().set_dipole_moment(*args, **kwargs)
 
     def set_nuclear_repulsion_energy(self, *args, **kwargs):
