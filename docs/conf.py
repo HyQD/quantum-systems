9d3b8865a87551e1847ed9b990283b02ee1c0521
gregwinther <sebastwi@uio.no>
2019-06-20 14:19:17 +0200

    Black.

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Quantum Systems'
-copyright = '2019, Schoyen, gregwinther'
-author = 'Schoyen, gregwinther'
+project = "Quantum Systems"
+copyright = "2019, Schoyen, gregwinther"
+author = "Schoyen, gregwinther"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,13 +30,13 @@ author = 'Schoyen, gregwinther'
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.mathjax", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -45,9 +45,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'classic'
+html_theme = "classic"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
