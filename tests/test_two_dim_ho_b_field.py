b2396719731428b401e4c07f5fa1e405aa1738b2
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-06-21 11:44:48 +0200

    Add test of two-dim ho with B-field

--- tests/test_two_dim_ho_b_field.py
+++ tests/test_two_dim_ho_b_field.py
@@ -1,8 +1,41 @@
+import os
+import pytest
 import numpy as np
 from quantum_systems.quantum_dots.two_dim.two_dim_ho import (
-    TwoDimensionalHarmonicOscillator as tdho,
-    TwoDimHarmonicOscB as tdho_b,
+    TwoDimensionalHarmonicOscillator,
+    TwoDimHarmonicOscB,
 )
+
+
+@pytest.fixture(scope="module")
+def get_tdhob():
+    n = 2
+    l = 20
+
+    radius = 5
+    num_grid_points = 1001
+    omega_c = 0.5
+
+    tdhob = TwoDimHarmonicOscB(n, l, radius, num_grid_points, omega_c=omega_c)
+    tdhob.setup_system()
+
+    return tdhob
+
+
+def test_tdhob(get_tdhob):
+    tdhob = get_tdhob
+
+    dip = np.load(os.path.join("tests", "dat", "tdhob_dipole_moment.npy"))
+    np.testing.assert_allclose(dip, tdhob.dipole_moment, atol=1e-10)
+
+    h = np.load(os.path.join("tests", "dat", "tdhob_h.npy"))
+    np.testing.assert_allclose(h, tdhob.h, atol=1e-10)
+
+    u = np.load(os.path.join("tests", "dat", "tdhob_u.npy"))
+    np.testing.assert_allclose(u, tdhob.u, atol=1e-10)
+
+    spf = np.load(os.path.join("tests", "dat", "tdhob_spf.npy"))
+    np.testing.assert_allclose(spf, tdhob.spf, atol=1e-10)
 
 
 def test_two_body_elements_compare():
@@ -14,11 +47,11 @@ def test_two_body_elements_compare():
     omega = 1
     omega_c = 0
 
-    two_dim_ho = tdho(
+    two_dim_ho = TwoDimensionalHarmonicOscillator(
         n, l, grid_length, num_grid_points, mass=mass, omega=omega
     )
     two_dim_ho.setup_system()
-    two_dim_ho_b = tdho_b(
+    two_dim_ho_b = TwoDimHarmonicOscB(
         n,
         l,
         grid_length,
