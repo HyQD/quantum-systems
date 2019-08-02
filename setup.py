e4c6d0c6b55a968b7cbc13513884ce17dd3bc387
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-02 10:40:25 +0200

    Fix build

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="Quantum systems", version="0.0.4", packages=find_packages()) 
+setup(name="Quantum systems", version="0.0.4", packages=find_packages())
