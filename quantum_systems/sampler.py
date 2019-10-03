5e4564ec763a49e6cbdbb0716ab17cbd16e8b883
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-10-03 15:39:28 +0200

    Return samples and optionally store data

--- quantum_systems/sampler.py
+++ quantum_systems/sampler.py
@@ -3,16 +3,16 @@ import abc
 
 
 class SampleCollector:
-    """Abstract base class defining common methods for all sampler classes. The
-    purpose of a derived class of `SampleCollector` is to define all sampler
-    classes and pass them to the constructor of `SampleCollector`.
+    """Base class defining common methods for all sampler classes. The purpose
+    of a derived class of ``SampleCollector`` is to define all sampler classes
+    and pass them to the constructor of ``SampleCollector``.
 
     Parameters
     ----------
     samplers : iterable
         Container with all sampler classes.
     np : linalg module
-        For example, `numpy` or `cupy`.
+        For example, ``numpy`` or ``cupy``.
     """
 
     def __init__(self, samplers, np):
@@ -36,33 +36,47 @@ class SampleCollector:
         for sampler in self.samplers:
             sampler.sample(step)
 
-    def dump(self, path="dump", filename=None, exist_ok=True):
+    def dump(
+        self, path="dump", filename=None, exist_ok=True, save_samples=True
+    ):
         """Function storing sampled data. As of now this function dumps the full
-        `samples`-dictionary using `np.save`.
+        ``samples``-dictionary using ``np.save``.
 
         Parameters
         ----------
         path : str
             Directory path to storage location. This path is created if it does
-            not exist. Default is `"dump"`.
+            not exist. Default is ``"dump"``.
         filename : str
-            Name of samples dictionary output. Default is `None` which results
-            in the name `"tmp_{self.__class__.__name__}.samples"`.
+            Name of samples dictionary output. Default is ``None`` which results
+            in the name ``"tmp_{self.__class__.__name__}.samples"``.
         exist_ok : bool
             Whether or not to create directory path if it already exists.
-            Default is `True` as the path does not overwrite any existing data.
+            Default is ``True`` as the path does not overwrite any existing data.
+        save_samples : bool
+            Whether or not to write the samples to disk. Default is ``True``.
+
+        Returns
+        -------
+        dict
+            Dictionary with all sampled values.
         """
+
+        for sampler in self.samplers:
+            self.samples = sampler.dump(self.samples)
+
+        if not save_samples:
+            return self.samples
 
         if filename is None:
             filename = f"tmp_{self.__class__.__name__}.samples"
 
         os.makedirs(path, exist_ok=exist_ok)
 
-        for sampler in self.samplers:
-            self.samples = sampler.dump(self.samples)
-
         filename = os.path.join(path, filename)
         self.np.save(filename, self.samples)
+
+        return self.samples
 
     def add_sample(self, key, sample):
         self.samples[key] = sample
