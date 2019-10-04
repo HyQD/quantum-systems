4d08a72e8787fa78ded252a41ad1ce4e4422b8e7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-10-04 11:38:48 +0200

    Update one-dim scripts

--- scripts/one_dim_ho.py
+++ scripts/one_dim_ho.py
@@ -1,13 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from quantum_systems import OneDimensionalHarmonicOscillator
-from quantum_systems.quantum_dots.one_dim.one_dim_potentials import (
-    HOPotential,
-    DWPotential,
-    GaussianPotential,
-    DWPotentialSmooth,
-)
+from quantum_systems import ODQD
 
 
 def save_data(system, system_name):
@@ -24,28 +18,26 @@ grid_length = 5
 num_grid_points = 1001
 omega = 1
 
-odho = OneDimensionalHarmonicOscillator(n, l, grid_length, num_grid_points)
-odho.setup_system(potential=HOPotential(omega))
+odho = ODQD(n, l, grid_length, num_grid_points)
+odho.setup_system(potential=ODQD.HOPotential(omega))
 save_data(odho, "odho")
 
 length_of_dw = 5
 
-oddw = OneDimensionalHarmonicOscillator(n, l, 6, num_grid_points)
-oddw.setup_system(potential=DWPotential(omega, length_of_dw))
+oddw = ODQD(n, l, 6, num_grid_points)
+oddw.setup_system(potential=ODQD.DWPotential(omega, length_of_dw))
 save_data(oddw, "oddw")
 
 weight = 1
 center = 0
 deviation = 2.5
 
-odgauss = OneDimensionalHarmonicOscillator(n, l, 20, num_grid_points)
+odgauss = ODQD(n, l, 20, num_grid_points)
 odgauss.setup_system(
-    potential=GaussianPotential(weight, center, deviation, np=np)
+    potential=ODQD.GaussianPotential(weight, center, deviation, np=np)
 )
 save_data(odgauss, "odgauss")
 
-oddw_smooth = OneDimensionalHarmonicOscillator(
-    n, l, grid_length, num_grid_points
-)
-oddw_smooth.setup_system(potential=DWPotentialSmooth(a=5))
+oddw_smooth = ODQD(n, l, grid_length, num_grid_points)
+oddw_smooth.setup_system(potential=ODQD.DWPotentialSmooth(a=5))
 save_data(oddw_smooth, "oddw_smooth")
