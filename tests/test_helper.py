0168dd534ec84c58c2fdab4512455f2894f9ce63
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-19 14:31:44 +0100

    Use updated classes for helper-tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,11 +1,6 @@
 import numpy as np
-from quantum_systems import QuantumSystem
-from quantum_systems.system_helper import (
-    add_spin_one_body,
-    add_spin_two_body,
-    spin_delta,
-    anti_symmetrize_u,
-)
+from quantum_systems import BasisSet
+from quantum_systems.system_helper import spin_delta
 
 
 def test_spin_delta():
@@ -19,60 +14,58 @@ def test_spin_delta():
 def test_transform_one_body_elements():
     l = 10
     h = np.random.random((l, l)) + 1j * np.random.random((l, l))
-    c = np.random.random((l, l)) + 1j * np.random.random((l, l))
+    C = np.random.random((l, l)) + 1j * np.random.random((l, l))
 
-    h_transformed = np.einsum("ip, jq, ij", c.conj(), c, h, optimize=True)
+    h_transformed = np.einsum("ip, jq, ij", C.conj(), C, h, optimize=True)
 
     np.testing.assert_allclose(
         h_transformed,
-        QuantumSystem.transform_one_body_elements(h, c, np=np),
+        BasisSet.transform_one_body_elements(h, C, np=np),
         atol=1e-10,
     )
 
-    _h = np.dot(h, c)
-    _h = np.dot(c.conj().T, _h)
+    _h = np.dot(h, C)
+    _h = np.dot(C.conj().T, _h)
 
     np.testing.assert_allclose(
-        _h, QuantumSystem.transform_one_body_elements(h, c, np)
+        _h, BasisSet.transform_one_body_elements(h, C, np)
     )
     np.testing.assert_allclose(
-        _h,
-        QuantumSystem.transform_one_body_elements(h, c, np, c_tilde=c.conj().T),
+        _h, BasisSet.transform_one_body_elements(h, C, np, C_tilde=C.conj().T),
     )
 
 
 def test_transform_two_body_elements():
     l = 10
     u = np.random.random((l, l, l, l)) + 1j * np.random.random((l, l, l, l))
-    c = np.random.random((l, l)) + 1j * np.random.random((l, l))
+    C = np.random.random((l, l)) + 1j * np.random.random((l, l))
 
     u_transformed = np.einsum(
         "ls, kr, jq, ip, ijkl -> pqrs",
-        c,
-        c,
-        c.conj(),
-        c.conj(),
+        C,
+        C,
+        C.conj(),
+        C.conj(),
         u,
         optimize=True,
     )
 
     np.testing.assert_allclose(
         u_transformed,
-        QuantumSystem.transform_two_body_elements(u, c, np=np),
+        BasisSet.transform_two_body_elements(u, C, np=np),
         atol=1e-10,
     )
 
-    _u = np.dot(u, c)
-    _u = np.tensordot(_u, c, axes=(2, 0)).transpose(0, 1, 3, 2)
-    _u = np.tensordot(_u, c.conj(), axes=(1, 0)).transpose(0, 3, 1, 2)
-    _u = np.tensordot(c.conj().T, _u, axes=(1, 0))
+    _u = np.dot(u, C)
+    _u = np.tensordot(_u, C, axes=(2, 0)).transpose(0, 1, 3, 2)
+    _u = np.tensordot(_u, C.conj(), axes=(1, 0)).transpose(0, 3, 1, 2)
+    _u = np.tensordot(C.conj().T, _u, axes=(1, 0))
 
     np.testing.assert_allclose(
-        _u, QuantumSystem.transform_two_body_elements(u, c, np)
+        _u, BasisSet.transform_two_body_elements(u, C, np)
     )
     np.testing.assert_allclose(
-        _u,
-        QuantumSystem.transform_two_body_elements(u, c, np, c_tilde=c.conj().T),
+        _u, BasisSet.transform_two_body_elements(u, C, np, C_tilde=C.conj().T),
     )
 
 
@@ -86,7 +79,9 @@ def test_add_spin_one_body():
         for q in range(l):
             h_spin[p, q] = spin_delta(p, q) * h[p // 2, q // 2]
 
-    np.testing.assert_allclose(h_spin, add_spin_one_body(h, np=np), atol=1e-10)
+    np.testing.assert_allclose(
+        h_spin, BasisSet.add_spin_one_body(h, np=np), atol=1e-10
+    )
 
 
 def test_spin_two_body():
@@ -105,7 +100,9 @@ def test_spin_two_body():
                         * u[p // 2, q // 2, r // 2, s // 2]
                     )
 
-    np.testing.assert_allclose(u_spin, add_spin_two_body(u, np=np), atol=1e-10)
+    np.testing.assert_allclose(
+        u_spin, BasisSet.add_spin_two_body(u, np=np), atol=1e-10
+    )
 
 
 def test_anti_symmetrize_u():
@@ -132,7 +129,9 @@ def test_anti_symmetrize_u():
                     )
 
     np.testing.assert_allclose(
-        u_spin, anti_symmetrize_u(add_spin_two_body(u, np=np)), atol=1e-10
+        u_spin,
+        BasisSet.anti_symmetrize_u(BasisSet.add_spin_two_body(u, np=np)),
+        atol=1e-10,
     )
 
 
@@ -141,7 +140,7 @@ def test_anti_symmetric_properties():
     u = np.random.random((l_half, l_half, l_half, l_half))
     # Make u symmetric
     u = u + u.transpose(1, 0, 3, 2)
-    u = anti_symmetrize_u(add_spin_two_body(u, np=np))
+    u = BasisSet.anti_symmetrize_u(BasisSet.add_spin_two_body(u, np=np))
 
     np.testing.assert_allclose(u, -u.transpose(0, 1, 3, 2), atol=1e-10)
     np.testing.assert_allclose(u, -u.transpose(1, 0, 2, 3), atol=1e-10)
