12ae6f318f52c339c360465528cebcda37c3117d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-19 14:26:38 +0100

    Import updated systems and classes

--- quantum_systems/__init__.py
+++ quantum_systems/__init__.py
@@ -1,13 +1,14 @@
 from .system import QuantumSystem
-from .spin_orbital_system import SpinOrbitalSystem
-from .orbital_system import OrbitalSystem
+from .general_orbital_system import GeneralOrbitalSystem
+from .spatial_orbital_system import SpatialOrbitalSystem
+from .basis_set import BasisSet
 from .custom_system import (
     # construct_psi4_system,
     # construct_pyscf_system,
     construct_pyscf_system_ao,
     construct_pyscf_system_rhf,
 )
-from .random_system import RandomSystem
+from .random_basis import RandomBasisSet
 from quantum_systems.quantum_dots.one_dim.one_dim_qd import ODQD
 from quantum_systems.quantum_dots.two_dim.two_dim_ho import (
     TwoDimensionalHarmonicOscillator,
