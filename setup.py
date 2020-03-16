4702076d99b632f4fa8e8d48362a3355345aa9ec
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-03-16 11:34:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum-systems",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["numpy", "scipy", "pandas", "numba", "pyscf", "sympy",],
 )
