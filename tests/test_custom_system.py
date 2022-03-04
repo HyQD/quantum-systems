0bbf5b89a02b4cc446c7a1e2ce770a59046a2e7e
haakoek <h.e.kristiansen@kjemi.uio.no>
2022-03-04 12:30:04 +0100

    remove old redundant comments

--- tests/test_custom_system.py
+++ tests/test_custom_system.py
@@ -4,9 +4,8 @@ import warnings
 from quantum_systems import (
     SpatialOrbitalSystem,
     GeneralOrbitalSystem,
-    # construct_psi4_system,
-    # construct_pyscf_system,
     construct_pyscf_system_rhf,
+    setup_basis_set,
 )
 from quantum_systems.random_basis import RandomBasisSet
 
@@ -67,25 +66,6 @@ def test_change_of_basis():
     assert all([new_l == s for s in gos.u.shape])
     np.testing.assert_allclose(h_gos, gos.h, atol=1e-12, rtol=1e-12)
     np.testing.assert_allclose(u_gos, gos.u, atol=1e-12, rtol=1e-12)
-
-
-# def test_psi4_construction():
-#     He = """
-#         He 0.0 0.0 0.0
-#         symmetry c1
-#     """
-#
-#     options = {"basis": "cc-pVDZ", "scf_type": "pk", "e_convergence": 1e-8}
-#
-#     try:
-#         system = construct_psi4_system(He, options)
-#         assert True
-#     except ImportError:
-#         warnings.warn("Unable to import psi4.")
-
-
-# def test_pyscf_construction():
-#     system = construct_pyscf_system("be 0 0 0")
 
 
 def test_reference_energy():
