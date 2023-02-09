dbad76c73bcccb7f6be20415f9191533a07ccfc3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2023-02-09 15:21:54 +0100

    More efficient usage of fixtures in test

--- tests/test_one_dim_qd.py
+++ tests/test_one_dim_qd.py
@@ -114,68 +114,33 @@ def get_oddw_smooth():
     return oddw_smooth
 
 
-def test_odho(get_odho):
-    odho = get_odho
-
-    dip = np.load(os.path.join("tests", "dat", "odho_dipole_moment.npy"))
-    np.testing.assert_allclose(dip, odho.position, atol=1e-10)
-
-    h = np.load(os.path.join("tests", "dat", "odho_h.npy"))
-    np.testing.assert_allclose(h, odho.h, atol=1e-10)
-
-    u = np.load(os.path.join("tests", "dat", "odho_u.npy"))
-    np.testing.assert_allclose(u, odho.u, atol=1e-10)
-
-    spf = np.load(os.path.join("tests", "dat", "odho_spf.npy"))
-    np.testing.assert_allclose(spf, odho.spf, atol=1e-10)
+@pytest.fixture
+def get_odqd_systems(get_odho, get_oddw, get_odgauss, get_oddw_smooth):
+    return [
+        ("odho", get_odho),
+        ("oddw", get_oddw),
+        ("odgauss", get_odgauss),
+        ("oddw_smooth", get_oddw_smooth),
+    ]
 
 
-def test_oddw(get_oddw):
-    oddw = get_oddw
+def test_odqd_systems(get_odqd_systems):
+    for sys in get_odqd_systems:
+        name, odqd = sys
 
-    dip = np.load(os.path.join("tests", "dat", "oddw_dipole_moment.npy"))
-    np.testing.assert_allclose(dip, oddw.position, atol=1e-10)
+        dip = np.load(os.path.join("tests", "dat", f"{name}_dipole_moment.npy"))
+        np.testing.assert_allclose(
+            np.abs(dip), np.abs(odqd.position), atol=1e-10
+        )
 
-    h = np.load(os.path.join("tests", "dat", "oddw_h.npy"))
-    np.testing.assert_allclose(h, oddw.h, atol=1e-10)
+        h = np.load(os.path.join("tests", "dat", f"{name}_h.npy"))
+        np.testing.assert_allclose(h, odqd.h, atol=1e-10)
 
-    u = np.load(os.path.join("tests", "dat", "oddw_u.npy"))
-    np.testing.assert_allclose(u, oddw.u, atol=1e-10)
+        u = np.load(os.path.join("tests", "dat", f"{name}_u.npy"))
+        np.testing.assert_allclose(np.abs(u), np.abs(odqd.u), atol=1e-10)
 
-    spf = np.load(os.path.join("tests", "dat", "oddw_spf.npy"))
-    np.testing.assert_allclose(spf, oddw.spf, atol=1e-10)
-
-
-def test_odgauss(get_odgauss):
-    odgauss = get_odgauss
-
-    dip = np.load(os.path.join("tests", "dat", "odgauss_dipole_moment.npy"))
-    np.testing.assert_allclose(dip, odgauss.position, atol=1e-10)
-
-    h = np.load(os.path.join("tests", "dat", "odgauss_h.npy"))
-    np.testing.assert_allclose(h, odgauss.h, atol=1e-10)
-
-    u = np.load(os.path.join("tests", "dat", "odgauss_u.npy"))
-    np.testing.assert_allclose(u, odgauss.u, atol=1e-10)
-
-    spf = np.load(os.path.join("tests", "dat", "odgauss_spf.npy"))
-    np.testing.assert_allclose(spf, odgauss.spf, atol=1e-10)
-
-
-def test_oddw_smooth(get_oddw_smooth):
-    oddw_smooth = get_oddw_smooth
-
-    dip = np.load(os.path.join("tests", "dat", "oddw_smooth_dipole_moment.npy"))
-    np.testing.assert_allclose(dip, oddw_smooth.position, atol=1e-10)
-
-    h = np.load(os.path.join("tests", "dat", "oddw_smooth_h.npy"))
-    np.testing.assert_allclose(h, oddw_smooth.h, atol=1e-10)
-
-    u = np.load(os.path.join("tests", "dat", "oddw_smooth_u.npy"))
-    np.testing.assert_allclose(u, oddw_smooth.u, atol=1e-10)
-
-    spf = np.load(os.path.join("tests", "dat", "oddw_smooth_spf.npy"))
-    np.testing.assert_allclose(spf, oddw_smooth.spf, atol=1e-10)
+        spf = np.load(os.path.join("tests", "dat", f"{name}_spf.npy"))
+        np.testing.assert_allclose(np.abs(spf), np.abs(odqd.spf), atol=1e-10)
 
 
 def test_anti_symmetric_two_body_symmetry_odho(get_odho):
